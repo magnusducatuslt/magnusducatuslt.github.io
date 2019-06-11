9753961833aafacf4e44fdcb3740feb583ef84a6
Your Name <you@example.com>
2019-06-11 22:02:53 +0300

    feat

--- README.md
+++
@@ -1,6 +0,0 @@
-# langTable
-
-Text project for responsive layout.
-It's basic project just clone: <tag>
-`https://github.com/magnusducatuslt/langTable.git`<tag>
-and join!
---
+++ test.js
@@ -0,0 +1,20 @@
+var div = document.createElement('div');
+div.innerHTML = localStorage['test'];
+document.getElementById('app').append(div);
+window.onmessage = function(e) {
+  var payload = JSON.parse(e.data);
+  console.log(payload);
+  switch (payload.method) {
+    case 'set':
+      localStorage.setItem(payload.key, JSON.stringify(payload.data));
+      break;
+    case 'get':
+      var parent = window.parent;
+      var data = localStorage.getItem(payload.key);
+      parent.postMessage(data, '*');
+      break;
+    case 'remove':
+      localStorage.removeItem(payload.key);
+      break;
+  }
+};
