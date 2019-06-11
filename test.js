ca8a23976a0ccf3778acec9818b12843f1dc35dd
Your Name <you@example.com>
2019-06-11 23:08:02 +0300

    feat

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-var div = document.createElement('div');
-div.innerHTML = localStorage['test'];
-document.getElementById('app').append(div);
+// var div = document.createElement('div');
+// div.innerHTML = localStorage['test'];
+// document.getElementById('app').append(div);
+console.log('loaded');
 window.onmessage = function(e) {
   var payload = JSON.parse(e.data);
   console.log(payload);
